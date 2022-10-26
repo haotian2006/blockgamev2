1e7592437f08da89ddfb097daeb51fa5e61eca51
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2022-10-26 13:26:34 -0700

    k

--- src/ReplicatedStorage/RenderHandler/Removehiddenblocks.lua
+++ src/ReplicatedStorage/RenderHandler/Removehiddenblocks.lua
@@ -2,7 +2,7 @@ local LocalizationService = game:GetService("LocalizationService")
 local self = {}
 local f,qf = pcall(require,game.ReplicatedStorage.QuickFunctions)
 local f,settings = pcall(require,game.ReplicatedStorage.GameSettings)
-function self.HideBlocks(cx,cz,chunks,blockstocheck,libs)
+function self.HideBlocks(cx,cz,chunks,blockstocheck,libs)--chunks 1 = middle 2 = +x 3 = -x 4 = +z 5 = -z
     local currentblockid 
     qf = qf or libs.QuickFunctions
     settings = settings or libs.GameSettings
@@ -10,9 +10,6 @@ function self.HideBlocks(cx,cz,chunks,blockstocheck,libs)
     local function checkblockinch(ch,x,y,z)
         local a = ch[qf.cbt("grid","1d",x,y,z)]
         return a
-    end
-    local function checksurroundingblocks(x,y,z)
-        
     end
     local function IsAnBorder(x,y,z)
         local walls,ammount = {},0
@@ -32,7 +29,36 @@ function self.HideBlocks(cx,cz,chunks,blockstocheck,libs)
             walls["z-1"] = true
             ammount+=1
         end
-        return ammount ~=0, walls
+        return walls,ammount
+    end
+    local function checksurroundingblocks(x,y,z)
+        local walls,ammount = IsAnBorder(x,y,z)
+        local check = 0
+        local sides = {}
+        if (not walls["x1"] and checkblockinch(chunks[1],x+1,y,z)) or (walls["x1"] and checkblockinch(chunks[2],x+1,y,z))  then
+            check +=1
+            sides['x1'] = true
+        end
+        if (not walls["x-1"] and checkblockinch(chunks[1],x-1,y,z)) or (walls["x-1"] and checkblockinch(chunks[3],x-1,y,z))  then
+            check +=1
+            sides['x-1'] = true
+        end
+        if (checkblockinch(chunks[1],x,y+1,z)) then
+            check +=1
+            sides['y1'] = true
+        end
+        if (checkblockinch(chunks[1],x,y-1,z))  then
+            check +=1
+            sides['y-1'] = true
+        end
+        if (not walls["z1"] and checkblockinch(chunks[1],x,y,z+1)) or (walls["z1"] and checkblockinch(chunks[4],x,y,z+1))  then
+            check +=1
+            sides['z1'] = true
+        end
+        if (not walls["z-1"] and checkblockinch(chunks[1],x,y,z-1)) or (walls["z-1"] and checkblockinch(chunks[5],x,y,z-z))  then
+            check +=1
+            sides['z-1'] = true
+        end
     end
     local function can(x,y,z)
         
