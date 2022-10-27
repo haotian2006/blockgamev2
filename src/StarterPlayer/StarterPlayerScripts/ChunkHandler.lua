6d3cc181b5ffb53f7ea026185a16c892ee8bac27
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2022-10-27 13:19:02 -0700

    k

--- src/StarterPlayer/StarterPlayerScripts/ChunkHandler.lua
+++ src/StarterPlayer/StarterPlayerScripts/ChunkHandler.lua
@@ -3,6 +3,10 @@ local qf = require(game.ReplicatedStorage.QuickFunctions)
 local blockdata = require(game.ReplicatedStorage.blockdata)
 local mulithandler = require(game.ReplicatedStorage.MultiHandler)
 local rendering = require(game.ReplicatedStorage.RenderHandler)
-
-
+function self.GetChunk(cx,cz)
+    return game.ReplicatedStorage.Events.GetChunk:InvokeServer(cx,cz)
+end
+function self.GetCleanedChunk(cx,cz)
+    return mulithandler.HideBlocks(cx,cz)
+end
 return self
