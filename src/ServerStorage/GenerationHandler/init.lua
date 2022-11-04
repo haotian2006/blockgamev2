b7e7c10cdd684477a4f86529ce390d54bfec7a4f
haotian2006 <haotian2090@gmail.com>
2022-11-04 11:14:05 -0700

    k

--- src/ServerStorage/GenerationHandler/init.lua
+++ src/ServerStorage/GenerationHandler/init.lua
@@ -14,9 +14,9 @@ local seed = 12345
 local max_height = 60
 local noiseScale2 = 60/2
 
-local maxwormlength = 30
+local maxwormlength = 120
 local maxwormsperchunk = 3
-local Resolution = 4
+
 function generation.RandNumber(x,y,seed,range)
 	range = range or 1
 	return math.clamp(math.round(math.abs(math.noise((x/y+y/x),seed))*10),0,range)
@@ -34,16 +34,43 @@ function generation.Noise(x, y, octaves, lacunarity, persistence, scale, seed)
 	end
 	return math.clamp(value, -1, 1)
 end
-function generation.CreateWorm(cx,cz)
-	local sx,sy,sz = math.random(0,7),math.random(0,127),math.random(0,7)
-	local ammount = math.random(0,3)
-	for i = 0,ammount-1 do
-		local WormCF = CFrame.new(qf.convertchgridtoreal(sx,sy,sz,true))
-		local maxlength = math.random(3,maxwormlength)
-		for i=0, maxlength do
-			local x,y,z = math.noise(WormCF.X/Resolution+.1,seed),math.noise(WormCF.Y/Resolution+.1,seed),math.noise(WormCF.Z/Resolution+.1,seed)
+local function roundpos(v3)
+	return Vector3.new(math.floor(v3.X+0.5),math.floor(v3.Y+0.5),math.floor(v3.Z+0.5))
+end
+local Resolution = 1
+function generation.CreateWorm(cx,cz) -- cx and cy is the chunk it is being generated in
+	--note that all math.random functions will be changed so it will be procedural generated instead
+	local sx,sy,sz = math.random(0,7),math.random(0,127),math.random(0,7)-- generate a random startposition
+	local ammount = math.random(0,3) -- how much branches from the startpos 
+	local worms = {}
+	print(ammount)
+	for ci = 1,ammount do
+		local c = BrickColor.random()-- doing this so each branch will be a diffrent color
+		local wormdata = {}
+		local WormP = CFrame.new(qf.convertchgridtoreal(cx,cz,sx,sy,sz,true))--[[convert it to grid positon from local grid ex: if cx is 0 and chgrid = 1 then grid = 1 
+																			 but if cx == 1 and chgrid = 1 then grid = 8 ]]
+		local maxlength = math.random(50,maxwormlength) --worm length
+		for i=1, maxlength do
+			local x,y,z = math.noise(WormP.X/Resolution+.1,seed+ci)+0.01,math.noise(WormP.Y/Resolution+.1,seed+ci)+0.01,math.noise(WormP.Z/Resolution+.1,seed+ci)+0.01 -- get a direaction
+			WormP = WormP*CFrame.Angles(x*(1+ci),y*(1+ci),z*(1+ci))*CFrame.new(0,0,-Resolution) -- adding ci so it adds an offset
+			local roundpos = roundpos(WormP.Position)--rounds to grid
+			wormdata[qf.cv3type("string",roundpos)] = false -- convert the vector3 to a string and store it in a dictionary
+
+			-- this parts for debugging/visual
+			local Part = Instance.new("Part")
+			Part.Anchored = true
+			if i == 1 then
+				Part.Material = Enum.Material.Neon -- marks the start
+			end
+			Part.Position = roundpos
+			Part.Parent = workspace
+			Part.BrickColor = c
+			Part.Size = Vector3.new(1,1,1)
+
 		end
+		table.insert(worms,wormdata)
 	end
+	return worms
 end
 function generation.GenerateTable(cx,cz)
 	local newtable = {}
