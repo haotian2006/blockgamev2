1a59322953f8a510a2cb71a11a238d301d593b65
haotian2006 <haotian2090@gmail.com>
2022-11-07 23:49:42 -0800

    as

--- src/ReplicatedStorage/GameSettings.lua
+++ src/ReplicatedStorage/GameSettings.lua
@@ -1,6 +1,6 @@
 local settings = {}
 settings.Version = 0.01
-settings.GridSize = 4
+settings.GridSize = 3
 settings.ChunkSize = Vector2.new(8,128)
 settings.LargeChunkSize = 30 -- n x n
 settings.MaxBuildHeight = 200
