48887685de959bc7168fb4eba0d0b7ab568361a7
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2022-11-21 14:14:40 -0800

    a

---
+++ src/ReplicatedStorage/EntityHandler.lua
@@ -0,0 +1,15 @@
+local entity = {}
+function entity.new(data)
+    
+end
+function entity.Create(type,data)
+    
+end
+function entity:GoTo(x,y,z)
+    
+end
+function entity:Jump(Height)
+    
+end
+
+return entity
