b9038433310b088639a3590e0031206c261cc01a
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2022-12-01 18:06:43 -0800

    a

--- src/StarterPlayer/StarterPlayerScripts/Controller.lua
+++ src/StarterPlayer/StarterPlayerScripts/Controller.lua
@@ -1,12 +1,15 @@
-local controlls = {pc = {},mode = 'pc',func = {}}
-controlls.pc.Keybinds = {
-    Foward = {'w'}
+local controls = {pc = {},mode = 'pc',func = {}}
+controls.pc.Keybinds = {
+    Foward = {'w',"Foward"}-- Name = {key,function}
 }
-controlls.KeysPressed = {}
-controlls.Render = {}
-
+controls.KeysPressed = {}
+controls.Render = {}
+controls.Functionsdown = {}
+local func = controls.func
+local Render = controls.Render
 local runservice = game:GetService("RunService")
 local uis = game:GetService("UserInputService")
+
 local function getkeyfrominput(input)
     if input.KeyCode then
         return input.KeyCode.Name
@@ -17,33 +20,41 @@ end
 uis.InputBegan:Connect(function(input, gameProcessedEvent)
     if gameProcessedEvent then return end 
     local key = getkeyfrominput(input)
-    controlls.KeysPressed[key] = key
-    if controlls[controlls.mode] then
-        for i,v in controlls[controlls.mode] do
-            if v[1] == key then
+    controls.KeysPressed[key] = key
+    if controls[controls.mode] then
+        for i,v in controls[controls.mode] do
+            local function second()
                 if v[2] then
                     if type(v[2]) == "string" then
-                        if controlls.func[v[2]] then
-                            controlls.func[v[2]]()
+                        if controls.func[v[2]] then
+                            task.spawn(controls.func[v[2]],key)
+                        else
+                            controls.Functionsdown[v[2]] = key
                         end
                     else
-                        v[2]()
+                        task.spawn(v[2],key)
                     end
                 end
-                break
             end
+            if v[1] == key then
+                second()
+            elseif type(v[1]) == "table" then
+                if table.find(v[1],key) then
+                    second()
+                end
+            end 
         end
     end
 end)
 uis.InputEnded:Connect(function(input, gameProcessedEvent)
     if gameProcessedEvent then return end 
     local key = getkeyfrominput(input)
-    controlls.KeysPressed[key] = nil
+    controls.KeysPressed[key] = nil
 end)
-function controlls.renderupdate(dt)
-    for i,v in controlls.Render do
+function controls.renderupdate(dt)
+    for i,v in controls.Render do
         task.spawn(v,dt)
     end
 end
-runservice.Stepped:Connect( controlls.renderupdate)
-return controlls
+runservice.Stepped:Connect(controls.renderupdate)
+return controls
