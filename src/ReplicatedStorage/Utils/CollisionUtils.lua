b2835396c60d429cdce182f7897acf5381a9dc9b
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2024-03-26 09:51:34 -0700

    update utisl

--- src/ReplicatedStorage/Utils/CollisionUtils.lua
+++ src/ReplicatedStorage/Utils/CollisionUtils.lua
@@ -3,5 +3,5 @@ local CollisonHandler = require(game.ReplicatedStorage.CollisionHandler)
 
 function Utils.doesBlockCollideWithEntityAt(block,at)
     local EntitiesAt = CollisonHandler.getEntitiesInBox(at, Vector3.new(3,3,3))
-    
+    return #EntitiesAt > 0 
 end
